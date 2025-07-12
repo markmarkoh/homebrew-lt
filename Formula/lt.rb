7dccbccc30c13ed9b4c8a33dbc3ea72db833b907
Mark DiMarco <mark.dimarco@gmail.com>
2025-07-12 10:47:31 -0500

    0.0.8 release

--- Formula/lt.rb
+++ Formula/lt.rb
@@ -5,8 +5,8 @@ class Lt < Formula
   desc "An unofficial TUI client for Linear.app issues"
   homepage "https://github.com/markmarkoh/lt"
   url "https://github.com/markmarkoh/lt/releases/latest/download/lt.tar.gz"
-  sha256 "c1e4481ffb216e6d23d3430c647ef004e985ad1f7d3b4e963884b8285cb9e431"
-  version "0.0.6"
+  sha256 "031332fa2fcca5ed5a3994da0ec76ae45eeb7df928ee9cecddd34e7f677adb8b"
+  version "0.0.8"
 
   def install
     bin.install "lt"
