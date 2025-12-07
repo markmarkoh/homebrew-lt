68743962f3ea599b30e0c6011f18afa21bd99561
Mark DiMarco <mark.dimarco@gmail.com>
2025-12-07 15:42:09 -0600

    Update sha256 checksum for lt.rb

--- Formula/lt.rb
+++ Formula/lt.rb
@@ -5,7 +5,7 @@ class Lt < Formula
   desc "An unofficial TUI client for Linear.app issues"
   homepage "https://github.com/markmarkoh/lt"
   url "https://github.com/markmarkoh/lt/releases/latest/download/lt.tar.gz"
-  sha256 "ef0d7e4b83ba61da68511c0beb554d31a1bfe34c88cbf4e748985d04523798f9"
+  sha256 "38553145a7d0f17badbf6005db51db6fb10ab930528abeff853cc802b584825d"
   version "0.0.11"
 
   def install
