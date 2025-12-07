b7eae43c1a625ef84cfc5975685e697b392d7ddf
Mark DiMarco <mark.dimarco@gmail.com>
2025-12-07 14:50:49 -0600

    Bump version from 0.0.9 to 0.0.11

--- Formula/lt.rb
+++ Formula/lt.rb
@@ -6,7 +6,7 @@ class Lt < Formula
   homepage "https://github.com/markmarkoh/lt"
   url "https://github.com/markmarkoh/lt/releases/latest/download/lt.tar.gz"
   sha256 "ef0d7e4b83ba61da68511c0beb554d31a1bfe34c88cbf4e748985d04523798f9"
-  version "0.0.9"
+  version "0.0.11"
 
   def install
     bin.install "lt"
